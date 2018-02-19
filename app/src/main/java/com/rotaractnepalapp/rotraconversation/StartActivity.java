f19ce6a8f482399405ecfeea8bdf7078964c1d38
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-02-19 14:15:14 +0545

    registration from layout uncomplete

--- app/src/main/java/com/rotaractnepalapp/rotraconversation/StartActivity.java
+++ app/src/main/java/com/rotaractnepalapp/rotraconversation/StartActivity.java
@@ -1,14 +1,29 @@
 package com.rotaractnepalapp.rotraconversation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button mregBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mregBtn = (Button) findViewById(R.id.startRegButton);
+
+        mregBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regIntent = new Intent(StartActivity.this,RegisterActivity.class);
+                startActivity(regIntent);
+            }
+        });
     }
 
 }
