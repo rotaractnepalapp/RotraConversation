abde634fe2cf77c1eb6ae0b1641247248777edb4
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-02-21 08:33:37 +0545

    connect firebase database and store user data

--- app/src/main/java/com/rotaractnepalapp/rotraconversation/RegisterActivity.java
+++ app/src/main/java/com/rotaractnepalapp/rotraconversation/RegisterActivity.java
@@ -16,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -25,6 +30,7 @@ public class RegisterActivity extends AppCompatActivity {
     private ProgressDialog mRegProgress;
 
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,17 +73,39 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void register_user(String display_name, String ridno, String email, String password) {
+    private void register_user(final String display_name, final String ridno, String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
-                    mRegProgress.dismiss();
-                    Toast.makeText(RegisterActivity.this,"Sucessful Register",Toast.LENGTH_LONG).show();
-                    Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(mainIntent);
-                    finish();
+
+                    //to get current user id from firebase
+                    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                    String uid = current_user.getUid();
+
+                    //to create user reg data into firebase --  FirebaseDatabase+DatabaseReference(Root)+child(users)+child(user id)
+                    mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                    HashMap<String, String> userMap = new HashMap<>();
+                    userMap.put("name", display_name);
+                    userMap.put("ridno", ridno);
+                    userMap.put("status", "Hi there I'm using Rotra Conversation App.");
+                    userMap.put("image", "default");
+                    userMap.put("thumb_image", "default");
+
+                    mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                mRegProgress.dismiss();
+                                Toast.makeText(RegisterActivity.this,"Sucessful Register",Toast.LENGTH_LONG).show();
+                                Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
+                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(mainIntent);
+                                finish();
+                            }
+                        }
+                    });
                 }else {
                     mRegProgress.hide();
                     Toast.makeText(RegisterActivity.this,"You got some error",Toast.LENGTH_LONG).show();
