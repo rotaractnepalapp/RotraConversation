e147fbe275aa35f34f7319fe4002e8d732177ea4
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-03-07 18:31:21 +0545

    design layout chat activity

--- app/src/main/java/com/rotaractnepalapp/rotraconversation/ChatActivity.java
+++ app/src/main/java/com/rotaractnepalapp/rotraconversation/ChatActivity.java
@@ -1,8 +1,12 @@
 package com.rotaractnepalapp.rotraconversation;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,14 +26,22 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         mChatToolbar = (Toolbar) findViewById(R.id.chat_toolbar);
+
         setSupportActionBar(mChatToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
 
         mRootRef = FirebaseDatabase.getInstance().getReference();
 
         mChatUser = getIntent().getStringExtra("user_id");
         String userName = getIntent().getStringExtra("user_name");
-        getSupportActionBar().setTitle(userName);
+        //getSupportActionBar().setTitle(userName);
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View action_bar_view = inflater.inflate(R.layout.chat_custom_bar, null);
+
+        actionBar.setCustomView(action_bar_view);
 
     }
 }
