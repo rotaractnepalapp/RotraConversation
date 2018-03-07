12ce4d9feacf55a30f4065bbbcba5594b4499823
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-03-07 07:09:20 +0545

    adding friends list on friends_fragment (incomplete)

--- app/src/main/java/com/rotaractnepalapp/rotraconversation/FriendsFragment.java
+++ app/src/main/java/com/rotaractnepalapp/rotraconversation/FriendsFragment.java
@@ -1,27 +1,41 @@
 package com.rotaractnepalapp.rotraconversation;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FriendsFragment extends Fragment {
 
     private RecyclerView mFriendsList;
-    private DatabaseReference mFriendsDatabase;
+    private DatabaseReference mFriendsDatabase, mUserDatabase;
     private FirebaseAuth mAuth;
 
     private String mCurrent_user_id;
 
     private View mMainView;
+
+    private FirebaseRecyclerAdapter<Friends, FriendsViewHolder> friendsRecyclerViewAdapter;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -40,21 +54,96 @@ public class FriendsFragment extends Fragment {
         mCurrent_user_id = mAuth.getCurrentUser().getUid();
 
         mFriendsDatabase = FirebaseDatabase.getInstance().getReference().child("Friends").child(mCurrent_user_id);
+        mFriendsDatabase.keepSynced(true);
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mUserDatabase.keepSynced(true);
 
         mFriendsList.setHasFixedSize(true);
         mFriendsList.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        FirebaseRecyclerOptions <Friends> options = new FirebaseRecyclerOptions.Builder<Friends>()
+                .setQuery(mFriendsDatabase,Friends.class)
+                .build();
+        friendsRecyclerViewAdapter = new FirebaseRecyclerAdapter<Friends, FriendsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final FriendsViewHolder holder, int position, @NonNull Friends model) {
+                holder.setDate(model.getDate());
+
+                String list_user_id = getRef(position).getKey();
+
+                mUserDatabase.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String userName = dataSnapshot.child("name").getValue().toString();
+                        String userThumb = dataSnapshot.child("thumb_image").getValue().toString();
+
+                        holder.setName(userName);
+                        holder.setUserImage(userThumb, getContext());
+                        holder.setRIDNo("Last message show here");
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public FriendsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.user_list_layout, parent, false);
+                return new FriendsViewHolder(view);
+            }
+        };
+        mFriendsList.setAdapter(friendsRecyclerViewAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        return mMainView;
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        friendsRecyclerViewAdapter.startListening();
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        if(friendsRecyclerViewAdapter != null) {
+            friendsRecyclerViewAdapter.stopListening();
+        }
+    }
+
+    public class FriendsViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        private CircleImageView mUserView;
+
+        public FriendsViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setDate(String date){
+            TextView userStatusView = (TextView) mView.findViewById(R.id.user_single_status);
+            userStatusView.setText(date);
+        }
+
+        public void setName(String name){
+            TextView userName = (TextView) mView.findViewById(R.id.user_single_name);
+            userName.setText(name);
+        }
+
+        public void setUserImage(String thumb_image, Context ctx){
+            CircleImageView userImageView = (CircleImageView) mView.findViewById(R.id.user_single_image);
+            Picasso.with(ctx).load(thumb_image).placeholder(R.drawable.default_avatar).into(userImageView);
+        }
+
+        public void setRIDNo(String ridno){
+            TextView userRIDNoView = (TextView) mView.findViewById(R.id.user_single_ridno);
+            userRIDNoView.setText(ridno);
+        }
     }
 }
